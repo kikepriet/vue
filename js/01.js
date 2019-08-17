592d7f70abb2cabdefe261e26f9260e3dd08dc0a
kike priet <kikepriet@gmail.com>
2019-08-17 18:35:06 -0500

    este es el primer commit de la saga de vue

---
+++ js/01.js
@@ -0,0 +1,31 @@
+// vamos a instanciar a con el id app
+const app = new Vue({
+    el: '#app',
+    data: {
+        titulo: 'Hola mundo con Vue',
+        frutas: [
+            {nombre:'Manzana', cantidad:10},
+            {nombre:'Pera', cantidad:0},
+            {nombre:'Platano', cantidad:11}
+        ],
+        nuevaFruta: '',
+        total: 0
+    },
+    methods:{
+        agregarFruta () {
+            this.frutas.push({
+                nombre: this.nuevaFruta, cantidad: 0
+            });
+            this.nuevaFruta = "";
+        }
+    },
+    computed: {
+        sumarFrutas () {
+            this.total = 0;
+            for (fruta of this.frutas){
+                this.total = this.total + fruta.cantidad;
+            }
+            return this.total;
+        }
+    }
+}) 
